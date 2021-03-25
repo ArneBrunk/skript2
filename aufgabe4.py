bf9fe0f59b1bd7b60e48bf3cbc197ed99d5fb6ef
Arne Brunkert <arnebrunkert@gmail.com>
2021-03-25 15:49:30 +0100

    commit Monster v1

--- aufgabe4.py
+++ aufgabe4.py
@@ -1,3 +1,29 @@
 from random import random
 
 dmg = random.randrange(1.10)
+
+class Monster_Zustand ():
+    pass
+
+class Mon_Normal():
+    pass
+class Mon_Rage():
+    pass
+class Mon_Dead():
+    pass
+
+class Monster ():
+    def __init__(self, name):
+        self.name = name
+        self.hp = 50
+        self.zustand = Mon_Normal()
+
+    
+
+class Hero():
+    def __init__(self,name):
+        self.name = name
+        self.hp = 50
+    def fight(self):
+        
+        
