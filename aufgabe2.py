97c972ae70fdd99876d5d7aeaddc9d3ff826fa68
Arne Brunkert <arnebrunkert@gmail.com>
2021-04-07 20:17:22 +0200

    commit state patern

--- aufgabe2.py
+++ aufgabe2.py
@@ -4,47 +4,40 @@ class IMove():
         raise NotImplementedError
 
 class diagonal(IMove):
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-    def move(self):
-        self.x += 5 
-        self.y += 5
-        print("%d,%d" %(self.x, self.y))
+    def move(self, alien):
+        alien.x += 5 
+        alien.y += 5
+        print("%d,%d" %(alien.x, alien.y))
 
 class up (IMove):
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-    def move(self):
-        self.y += 5
-        print("%d,%d" %(self.x, self.y))
+    def move(self, alien):
+        alien.y += 5
+        print("%d,%d" %(alien.x, alien.y))
 
 class normal (IMove):
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-    def move(self):
-        self.x += 5 
-        print("%d,%d" %(self.x, self.y))
+    def move(self, alien):
+        alien.x += 5 
+        print("%d,%d" %(alien.x, alien.y))
 
 class Alien ():
     def __init__(self, name, mode: IMove):
         self.name = name
         self._mode = mode
+        self.x = 0
+        self.y = 0
         
 
     def move (self):
-        self._mode.move()
+        self._mode.move(self)
         
 
 
 alien1 = Alien("Harald", normal())
 alien1.move()
 alien1.move()
-alien2 = Alien("Harry", diagonal())
-alien2.move()
-alien2.move()
-alien3 = Alien("Howard", up())
-alien3.move()
-alien3.move()
+alien1._mode = diagonal()
+alien1.move()
+alien1.move()
+alien1._mode = up()
+alien1.move()
+alien1.move()
